0111747a0d558f2c07967d5b901ed087a9721ba0
jiahaiyong <jiahaiyong@qimai.net>
2018-10-18 20:21:58 +0800

    个人页面

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -33,8 +33,16 @@ Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm
 Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
 /**
+ *
  * make:auth route
  */
 //Auth::routes();
 //
 //Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+
+//Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+//Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+//Route::patch('/users/{user}', 'UsersController@update')->name('users.update');

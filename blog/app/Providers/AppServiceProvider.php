366fe78031efdc12745f187105bf105d35f1bd74
jiahaiyong <jiahaiyong@qimai.net>
2018-11-20 19:24:50 +0800

    站点权限部署

--- blog/app/Providers/AppServiceProvider.php
+++ blog/app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Carbon\Carbon;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Log;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -30,5 +31,9 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+        if (app()->isLocal()) {
+            $this->app->register(\VIACreative\SudoSu\ServiceProvider::class);
+        }
+
     }
 }

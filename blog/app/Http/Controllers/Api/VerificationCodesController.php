43beafcd91aed8a23acd21d546e19aeca49dcc8c
jiahaiyong <jiahaiyong@qimai.net>
2018-11-29 10:59:09 +0800

    图片验证码

--- blog/app/Http/Controllers/Api/VerificationCodesController.php
+++ blog/app/Http/Controllers/Api/VerificationCodesController.php
@@ -10,10 +10,21 @@ class VerificationCodesController extends Controller
 {
     public function store(VerificationCodeRequest $request, EasySms $easySms)
     {
+        $captchaData = \Cache::get($request->captcha_key);
+
+        if (!$captchaData) {
+            return $this->response->error('图片验证码已失效', 422);
+        }
+
+        if (!hash_equals($captchaData['code'], $request->captcha_code)) {
+            // 验证错误就清除缓存
+            \Cache::forget($request->captcha_key);
+            return $this->response->errorUnauthorized('验证码错误');
+        }
 
         $phone = $request->phone;
 
-        if (app()->environment('production')) {
+        if (!app()->environment('production')) {
             echo app()->environment('production');
         } else {
 

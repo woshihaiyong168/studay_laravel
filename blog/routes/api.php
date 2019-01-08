7c9521a6e85556c6c8b771df1580394bad791ed1
jiahaiyong <jiahaiyong@qimai.net>
2019-01-08 15:45:02 +0800

    资源推荐接口

--- blog/routes/api.php
+++ blog/routes/api.php
@@ -71,6 +71,9 @@ $api->version('v1', [
         $api->get('users/{user}/replies', 'RepliesController@userIndex')
             ->name('api.users.replies.index');
 
+        // 资源推荐
+        $api->get('links', 'LinksController@index')
+            ->name('api.links.index');
 
 
 
